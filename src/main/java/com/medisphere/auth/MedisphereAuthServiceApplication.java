1c5ca76d56d4e45dcd3e87912dd9411af9e93eb9
AroshXR <aroshanasandeep454@gmail.com>
2026-04-05 22:23:31 +0530

    application properties

--- src/main/java/com/medisphere/auth/MedisphereAuthServiceApplication.java
+++ src/main/java/com/medisphere/auth/MedisphereAuthServiceApplication.java
@@ -1,9 +1,10 @@
 package com.medisphere.auth;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MedisphereAuthServiceApplication {
 
 	public static void main(String[] args) {
